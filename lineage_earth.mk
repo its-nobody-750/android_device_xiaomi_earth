2943801d2731a6e0d4d61ae01533aef29efc65e5
π <dinarysf276@gmail.com>
2025-10-16 19:34:32 +0700

    Update lineage_earth.mk

--- lineage_earth.mk
+++ lineage_earth.mk
@@ -14,6 +14,10 @@ $(call inherit-product, device/xiaomi/earth/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Bootanimation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1650
+
 PRODUCT_NAME := lineage_earth
 PRODUCT_DEVICE := earth
 PRODUCT_MANUFACTURER := Xiaomi
