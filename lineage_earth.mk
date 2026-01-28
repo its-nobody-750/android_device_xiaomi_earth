2c998e6ce84eda2dbe2f35c79d6de9478cf8b1bb
Mr Nobody <140795564+its-nobody-750@users.noreply.github.com>
2026-01-29 00:17:28 +0530

    Update lineage_earth.mk

--- lineage_earth.mk
+++ lineage_earth.mk
@@ -27,8 +27,28 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="earth-user 14 UP1A.230105.007 V816.0.12.0.UCVMIXM release-keys" \
-    BuildFingerprint=Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.12.0.UCVMIXM:user/release-keys
+    BuildFingerprint=Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.12.0.UCVMIXM:user/release-keys \
     SystemModel=$(PRODUCT_SYSTEM_DEVICE) \
     SystemName=$(PRODUCT_SYSTEM_NAME) \
     ProductModel=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+
+#Axion Stuffs
+TARGET_ENABLE_BLUR := true
+# Camera information (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 50
+AXION_CAMERA_FRONT_INFO := 5
+
+# Maintainer name (underscores become spaces in the UI)
+AXION_MAINTAINER := Micaiah
+
+# Processor name (underscores become spaces)
+AXION_PROCESSOR := Helio_G85
+
+TARGET_INCLUDES_LOS_PREBUILTS := false
+
+# CPU governor support
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
+PERF_ANIM_OVERRIDE := false
+
